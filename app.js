5093fccd3e81c748ba37ec311557b514bdfc8d19
Jeff Harman <jrharman7@gmail.com>
2016-03-05 16:01:23 -0700

    started

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-angular.module('quizApp',[]);
+var app = angular.module('quizApp',['ui.router']);
+
+
+app.config(  function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+  $stateProvider.state('home',{
+    url:'/',
+    templateUrl: '/components/home/homeView.html',
+    controller: 'homeCtrl',
+    resolve: {
+      quizList: function (quizService) {
+        return quizService.getQuizNames();
+      }
+    }
+  });
+
+})
--- components/home/homeCtrl.js
+++ components/home/homeCtrl.js
@@ -1,5 +1,5 @@
 var app = angular.module('quizApp');
-app.controller('homeCtrl', function ($scope) {
-
-  
+app.controller('homeCtrl', function ($scope, quizList) {
+  console.log(pastQuizList);
+  $scope.quizzes = quizList;
 })
